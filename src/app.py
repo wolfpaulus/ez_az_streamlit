5b3a61b3a5af1b10f9b110a08306585673a8caf5
Wolf Paulus <wolf@paulus.com>
2024-01-07 13:15:39 -0700

    add the test workflow, for linting and testing on github

--- src/app.py
+++ src/app.py
@@ -5,14 +5,14 @@
 from random import randint
 import streamlit as st
 
-def ui(items: [int])->None: 
-    
+
+def ui(items: [int]) -> None:
+
     st.title("Streamlit Demo")
     st.subheader(".. on Azure")
     st.line_chart(items)
 
 
 if __name__ == "__main__":
-    data = [randint(0,10) for _ in range(25)]
+    data = [randint(0, 10) for _ in range(25)]
     ui(data)
-
